ee92a450577afdfd8afaa47b66d2ddcf7f2fc5c1
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-09-27 13:34:34 -0500

    Error Handling

--- app.js
+++ app.js
@@ -8,4 +8,18 @@ const app = express()
 app.use('/products', productsRoutes)
 app.use('/orders', ordersRoutes)
 
+app.use((request, response, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, request, response, next) => {
+  response.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
 module.exports = app
