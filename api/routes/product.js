71351ba69a28104e9647cafc779eae5486a9d795
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-09-27 18:57:05 -0500

    Product Model & CRUD operations

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,49 +1,94 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('../models/product')
+
+
 const router = express.Router()
 
-
 router.get('/', (request, response, next) => {
-  response.status(200).json({
-    message: 'Handle GET requests to /products'
-  })
+  Product.find().exec()
+    .then(docs => {
+      console.log(docs)
+      response.status(200).json(docs)
+    })
+    .catch(err => {
+      console.log(err)
+      response.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.post('/', (request, response, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: request.body.name,
     price: request.body.price
-  }
-  response.status(201).json({
-    message: 'Handle POST requests to /products',
-    created: product
   })
+  product.save()
+    .then(result => {
+      console.log(result)
+      response.status(201).json({
+        createdProduct: product
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      response.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.get('/:id', (request, response, next) => {
   const id = request.params.id
-  if (id === '1' || id === '2') {
-    response.status(200).json({
-      message: 'The id is ' + id
+  Product.findById(id).exec()
+    .then(doc => {
+      console.log(doc)
+      response.status(200).json(doc)
     })
-  } else {
-    response.status(200).json({
-      message: 'Not found'
+    .catch(err => {
+      console.log(err)
+      response.status(500).json({
+        error: err
+      })
     })
-  }
 })
 
 router.patch('/:id', (request, response, next) => {
   const id = request.params.id
-  response.status(200).json({
-    message: 'Updated product with id ' + id
-  })
+
+  // Extract the properties from request 
+  const propsToUpdate = {}
+  for (let prop of request.body) {
+    propsToUpdate[prop.propName] = prop.value
+  }
+
+  Product.update({ _id: id }, { $set: propsToUpdate }).exec()
+    .then(result => {
+      response.status(200).json({result})
+    })
+    .catch(err => {
+      console.log(err)
+      response.status(500).json({
+        error: err
+      })
+    })
+  
 })
 
 router.delete('/:id', (request, response, next) => {
   const id = request.params.id
-  response.status(200).json({
-    message: 'Deleted product with id ' + id
-  })
+  Product.remove({ _id: id }).exec()
+    .then(result => {
+      response.status(201).json({result})
+    })
+    .catch(err => {
+      console.log(err)
+      response.status(500).json({
+        error: err
+      })
+    })
 })
 
 module.exports = router
